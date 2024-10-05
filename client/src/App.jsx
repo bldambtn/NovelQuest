d7470f1aa8741ba963ffe733f3a10b179cae338d
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-10-05 16:16:47 -0500

    updated front-end specifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,25 @@
-import "./App.css";
-import { Outlet } from "react-router-dom";
-
-// Import Apollo Client
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"; 
+import React from "react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { Routes, Route } from "react-router-dom"; // <-- Make sure to import these
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
-// Create Apollo Client instance
 const client = new ApolloClient({
-  // Your GraphQL endpoint
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      {" "}
-      {/* Wrap your app with ApolloProvider */}
-      <>
-        <Navbar />
-        <Outlet />
-      </>
+      <Navbar />
+      <Routes>
+        {" "}
+        {/* Use the Routes component here */}
+        <Route path="/" element={<SearchBooks />} />
+        <Route path="/saved" element={<SavedBooks />} />
+      </Routes>
     </ApolloProvider>
   );
 }
