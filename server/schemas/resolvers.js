6e19400a1131078285257d06c1eaa8fa2e80c791
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-10-05 15:22:25 -0500

    Updated JWT info

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 // Imports
 const { AuthenticationError } = require("apollo-server-express"); 
-const User = require("./models/User"); 
-const { signToken } = require("./utils/auth"); 
+const User = require("../models/User"); 
+const { signToken } = require("../utils/auth"); 
 
 const resolvers = {
   Query: {
