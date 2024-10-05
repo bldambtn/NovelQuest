481683575ed94ecb57c31e9925de9768f3cc008a
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-10-05 13:25:53 -0500

    updated files related to swapping over to apollo server

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,71 @@
+// Imports
+const { AuthenticationError } = require("apollo-server-express"); 
+const User = require("./models/User"); 
+const { signToken } = require("./utils/auth"); 
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        // Find the user by ID and return their data
+        return await User.findById(context.user._id).select("-__v -password");
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (
+      parent,
+      { bookId, title, authors, description, image, link },
+      context
+    ) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          {
+            $addToSet: {
+              savedBooks: { bookId, title, authors, description, image, link },
+            },
+          },
+          // Return the updated user
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          {
+            $pull: { savedBooks: { bookId } },
+          },
+          // Return the updated user
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,55 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+require("dotenv").config(); 
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { AuthenticationError } = require("apollo-server-express");
+const jwt = require("jsonwebtoken");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; 
+const secret = process.env.SECRET;
 
+// Middleware for parsing request bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+const getUser = (token) => {
+  if (token) {
+    try {
+      return jwt.verify(token, secret);
+    } catch (e) {
+      throw new AuthenticationError("Invalid token");
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    const token = req.headers.authorization || "";
+    const user = getUser(token);
+    return { user };
+  },
+});
+
+// Apply Apollo middleware to the Express app
+server.applyMiddleware({ app });
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// Use defined routes
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// Connect to the database and start the server
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`);
+  });
 });
