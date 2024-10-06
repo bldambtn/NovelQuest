8e5dd27cd2bbdac65a74590e1691f9a00ff28e17
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-10-06 17:56:29 -0500

    another attempt

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
+const path = require("path"); // You need to add this line
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,15 +18,16 @@ async function startApolloServer() {
   await server.start();
   server.applyMiddleware({ app });
 
-  // Set security headers to manage CSP
+  // Set security headers for Content Security Policy (CSP)
   app.use((req, res, next) => {
     res.setHeader(
       "Content-Security-Policy",
-      "default-src 'self'; script-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline';"
+      "default-src 'self'; script-src 'self'; img-src 'self' data:; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; font-src 'self' https://fonts.gstatic.com;"
     );
-    return next();
+    next();
   });
 
+  // Serve static assets in production
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/build")));
   }
