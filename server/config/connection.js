02f8ba67cd7646994fad39a6967511f92c701e66
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-10-09 20:04:24 -0500

    updated connection.js file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
+// Define the connection URI for MongoDB
+const connection = mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
+// Export the connection
 module.exports = connection;
